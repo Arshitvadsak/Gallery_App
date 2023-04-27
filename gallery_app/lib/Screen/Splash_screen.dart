161e7adc9e1a4658104a13b6a5103ce4b814893e
Arshitvadsak <arshitvadsak@gmail.com>
2023-04-27 10:36:56 +0530

    second update

--- gallery_app/lib/Screen/Splash_screen.dart
+++ gallery_app/lib/Screen/Splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
 import '../main.dart';
 
@@ -16,11 +17,16 @@ class _SplashPageState extends State<SplashPage> {
   void initState() {
     super.initState();
     Timer(
-        Duration(seconds: 4),
-        () => Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => MyHomePage(),)));
+      Duration(seconds: 4),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MyHomePage(),
+        ),
+      ),
+    );
   }
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -29,35 +35,21 @@ class _SplashPageState extends State<SplashPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Spacer(),
+            Lottie.network(
+                'https://assets1.lottiefiles.com/packages/lf20_xYheUY49Cu.json',
+                height: 200),
+            Spacer(),
+            Text(
+              "Gallery App",
+              style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black38),
+            ),
             SizedBox(
-              height: size.height * 0.35,
+              height: 10,
             ),
-            Image(
-              image: AssetImage('assets/images/logo.png'),
-              height: 100,
-            ),
-            SizedBox(
-              height: size.height * 0.2,
-            ),
-            // LoadingAnimationWidget.twistingDots(
-            //   leftDotColor: const Color(0xFF1A1A3F),
-            //   rightDotColor: Colors.blueAccent,
-            //   size: size.height * 0.06,
-            // ),
-            SizedBox(
-              height: size.height * 0.1,
-            ),
-            Hero(
-              tag: 'title',
-              child: Text(
-                "Galleria",
-                style: 
-                     TextStyle(
-                  fontSize: 45,
-                      color: Get.isDarkMode?Colors.white24:Colors.black26,
-                )
-              ),
-            )
           ],
         ),
       ),
